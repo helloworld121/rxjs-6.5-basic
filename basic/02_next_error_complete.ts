a09fb1f0ae94586b3b5f00e21e8365223d28aff5
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2020-12-26 21:15:40 +0100

    example 4 next error complete

---
+++ basic/02_next_error_complete.ts
@@ -0,0 +1,19 @@
+import { concat, from, throwError } from 'rxjs';
+
+// "from" creates a new Observable
+//    => those 2 Observable should be played one after the other using "concat"
+//       just after the first completes the next will be send
+//       this shows also how to bring two stream together
+// "concat" gonna take the next Observable after the current one completes
+const $source = concat(
+    from([10, 20, 30]),
+    // "throwError" creates a stream that just throws an error
+    // throwError('This is an error message!'), // x
+    from([40, 50, 60]),
+);
+
+$source.subscribe({
+    next: v => console.log('next:', v),
+    error: err => console.log('error:', err),
+    complete: () => console.log('complete!'),
+});
