ebab2d47262fe6770e87d7bee41739fa17cdfd23
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2020-12-26 20:56:09 +0100

    adding some documentation

--- basic/01_init_observable.ts
+++ basic/01_init_observable.ts
@@ -1,11 +1,18 @@
 import { from, Observable } from 'rxjs';
 
-// Observable constructor
+// each Observer has three events: next, error, complete
+// next => next value
+// error => thrown by the observable => observers will auto-unsubscribe AND observable won't complete
+// complete => observable completes without error => no more values will be emitted AND observers will auto-unsubscribe
+
+// 1) Observable constructor
 const $source = new Observable(observer => {
     observer.next('Value 1');
     observer.next('Value 2');
+    // complete observable
     observer.complete();
 
+    // cleanup function
     return () => {
         console.log('observer unsubscribed');
     };
@@ -13,7 +20,7 @@ const $source = new Observable(observer => {
 
 $source.subscribe(v => console.log('Received Value:', v));
 
-// Creation operator
+// 2) Creation operator => alternative to 1) without cleanup function
 const $source2 = from(['Value 1', 'Value 2']);
 
 $source2.subscribe(v => console.log('Received Value:', v));
