ffdd800e5f229b30d85c20a08cf8fd6237289053
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2020-12-29 11:33:07 +0100

    example for 'asObservable'

---
+++ advanced/01_publish_subject.ts
@@ -0,0 +1,16 @@
+import {Observable, Subject} from "rxjs";
+
+// if we have a subject defined EVERYONE can call the method "next" on this subject
+// to avoid this we can use "asObservable"
+
+const $source = new Subject<string>();
+
+// 1) this way the observable can be subscribed, which don't provide "next"
+const $observable1 = $source.asObservable();
+
+// 2) type the provided observable
+const $observable2: Observable<string> = $source;
+const $observable3 = $source as Observable<string>;
+// but this way it is still a Subject
+($observable2 as Subject<string>).next('my-string');
+
